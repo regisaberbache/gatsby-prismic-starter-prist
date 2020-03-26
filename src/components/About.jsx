02c75c7985fd3440400891a9c39201224e2ba6e7
regisaberbache <10544627+regisaberbache@users.noreply.github.com>
2020-03-26 11:22:17 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const AboutContainer = styled("div")`
     }
 
     @media(max-width: ${dimensions.maxwidthMobile}px) {
-        grid-template-columns: 1fr 1fr;
+        grid-template-columns: 7em 1fr;
         grid-template-rows: 3em 1fr;
         grid-gap: 2em;
     }
