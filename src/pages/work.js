5d90a04af32b8c788876d4dda235e4122501612a
regisaberbache <10544627+regisaberbache@users.noreply.github.com>
2020-03-29 20:15:30 +0200

    Update work.js

--- src/pages/work.js
+++ src/pages/work.js
@@ -13,8 +13,8 @@ const WorkTitle = styled("h1")`
 const Work = ({ projects, meta }) => (
     <>
         <Helmet
-            title={`Projets`}
-            titleTemplate={`%s | Projets`}
+            title={`Régis Aberbache | Portfolio`}
+            titleTemplate={`Régis Aberbache | Portfolio`}
             meta={[
                 {
                     name: `description`,
