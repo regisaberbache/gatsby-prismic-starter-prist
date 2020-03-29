f4b55114b8b41ebc59f089051b148c6cee6ea961
regisaberbache <10544627+regisaberbache@users.noreply.github.com>
2020-03-29 19:07:21 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,6 +15,63 @@ const HeaderContent = styled("div")`
     justify-content: space-between;
 `
 
+const HeaderLogo = styled("div")`
+    display: grid;
+    grid-template-columns: repeat(2, auto);
+    grid-gap: 7em;
+    justify-content: flex-start;
+    width: 100%;
+
+    @media(max-width: ${dimensions.maxwidthTablet}px) {
+        grid-gap: 5.5em;
+    }
+
+    @media(max-width: ${dimensions.maxwidthMobile}px) {
+        grid-gap: 2.5em;
+    }
+
+    a {
+        color: currentColor;
+        text-decoration: none;
+        border-bottom: 3px solid transparent;
+        font-weight: 600;
+        font-size: 0.95em;
+        height: 100%;
+        padding-bottom: 1.25em;
+        padding-top: 0.25em;
+        display: block;
+        position: relative;
+
+        &:after {
+            position: absolute;
+            content: "";
+            bottom: 0;
+            width: 18px;
+            height: 3px;
+            background: transparent;
+            bottom: -3px;
+            right: 50%;
+            margin-right: -9px;
+            transition: 100ms ease-in-out background;
+        }
+
+        &:hover {
+            &:after {
+                background: ${colors.blue500};
+                transition: 100ms ease-in-out background;
+            }
+        }
+
+        &.Link--is-active {
+            &:after {
+                background: ${colors.blue500};
+                transition: 100ms ease-in-out background;
+            }
+        }
+    }
+`
+
+
 const HeaderLinks = styled("div")`
     display: grid;
     grid-template-columns: repeat(2, auto);
@@ -72,17 +129,18 @@ const HeaderLinks = styled("div")`
     }
 `
 
+
 const Header = () => (
     <HeaderContainer>
         <HeaderContent>
-
-            
-            <HeaderLinks>
+            <HeaderLogo>
                 <Link
                     activeClassName="Link--is-active"
                     to="/">
                     Régis Aberbache
                 </Link>
+            </HeaderLogo>         
+            <HeaderLinks>
                 <Link
                     activeClassName="Link--is-active"
                     to="/work">
