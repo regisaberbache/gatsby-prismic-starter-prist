e1afd983f8d5dd9d1c5e11a3bfc5ab22f792b3f6
regisaberbache <10544627+regisaberbache@users.noreply.github.com>
2020-03-30 00:19:23 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,8 @@ const HeaderLogo = styled("div")`
         text-decoration: none;
         border-bottom: 3px solid transparent;
         font-weight: 600;
-        font-size: 0.95em;
+        font-size: 1.4em;
+        line-height: 1.3em;
         height: 100%;
         padding-bottom: 1.25em;
         padding-top: 0.25em;
@@ -93,7 +94,8 @@ const HeaderLinks = styled("div")`
         text-decoration: none;
         border-bottom: 3px solid transparent;
         font-weight: 600;
-        font-size: 0.95em;
+        font-size: 1.4em;
+        line-height: 1.3em;
         height: 100%;
         padding-bottom: 1.25em;
         padding-top: 0.25em;
