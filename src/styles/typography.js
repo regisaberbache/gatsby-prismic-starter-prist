8c06b210acf3352d5ff93d8f80125ff7231a3947
Régis Aberbache <10544627+regisaberbache@users.noreply.github.com>
2020-04-03 19:51:46 +0200

    Change typo

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,13 +1,13 @@
-import "circular-std";
 import css from '@emotion/css';
 import dimensions from "styles/dimensions";
 
 const typeStyles = css`
     h1 {
-        font-family: 'CircularStd', sans-serif;
-        font-size: 2.8em;
-        line-height: 1.3;
+        font-family: serif;
+        font-size: 3em;
+        line-height: 1.2;
         letter-spacing: -0.02em;
+        text-shadow: 1px 1px 0 #FFEC59, 2px 2px 0 #FFEC59, 3px 3px 0 #FFEC59, 4px 4px 0 #FFEC59, 5px 5px 0 #FFEC59;
 
         @media(max-width:${dimensions.maxwidthTablet}px) {
             font-size: 2.25em;
@@ -19,6 +19,7 @@ const typeStyles = css`
     }
 
     h2 {
+        font-family: sans-serif;
         margin-bottom: 2rem;
         font-size: 1.9em;
         line-height: 1.1;
@@ -26,12 +27,14 @@ const typeStyles = css`
     }
 
     h3 {
+        font-family: sans-serif;
         line-height: 1.2;
         font-size: 1.5em;
         letter-spacing: -0.02em;
     }
 
     h5 {
+        font-family: sans-serif;
         font-family: sans-serif;
         margin-bottom: 1.45rem;
         font-weight: 500;
@@ -52,6 +55,7 @@ const typeStyles = css`
     }
 
     a {
+        font-family: sans-serif;
         letter-spacing: -0.02em;
         &:hover {
             cursor: pointer;
