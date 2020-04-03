aec35e8042d015fa16089be9d1595a439a31c493
Régis Aberbache <10544627+regisaberbache@users.noreply.github.com>
2020-04-03 22:57:26 +0200

    Update typography.js

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -3,11 +3,10 @@ import dimensions from "styles/dimensions";
 
 const typeStyles = css`
     h1 {
-        font-family: serif;
-        font-size: 3em;
+        font-family: 'CircularStd', sans-serif;
+        font-size: 2.8em;
         line-height: 1.2;
         letter-spacing: -0.02em;
-        text-shadow: 1px 1px 0 #FFEC59, 2px 2px 0 #FFEC59, 3px 3px 0 #FFEC59, 4px 4px 0 #FFEC59, 5px 5px 0 #FFEC59;
 
         @media(max-width:${dimensions.maxwidthTablet}px) {
             font-size: 2.25em;
@@ -19,7 +18,7 @@ const typeStyles = css`
     }
 
     h2 {
-        font-family: sans-serif;
+        font-family: 'CircularStd', sans-serif;
         margin-bottom: 2rem;
         font-size: 1.9em;
         line-height: 1.1;
@@ -27,15 +26,14 @@ const typeStyles = css`
     }
 
     h3 {
-        font-family: sans-serif;
+        font-family: 'CircularStd', sans-serif;
         line-height: 1.2;
         font-size: 1.5em;
         letter-spacing: -0.02em;
     }
 
     h5 {
-        font-family: sans-serif;
-        font-family: sans-serif;
+        font-family: 'CircularStd', sans-serif;
         margin-bottom: 1.45rem;
         font-weight: 500;
         line-height: 20px;
@@ -55,7 +53,7 @@ const typeStyles = css`
     }
 
     a {
-        font-family: sans-serif;
+        font-family: 'CircularStd', sans-serif;
         letter-spacing: -0.02em;
         &:hover {
             cursor: pointer;
