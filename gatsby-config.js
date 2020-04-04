77490946916336e74d19dbb5bb99cd3d503394f9
Régis Aberbache <10544627+regisaberbache@users.noreply.github.com>
2020-04-04 16:27:10 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,7 @@ module.exports = {
         options: {
             repositoryName: 'regisportfolio', // (REQUIRED, replace with your own)
             linkResolver: () => post => `/${post.uid}`,
+            omitPrismicScript: true
         }
     },
     {
